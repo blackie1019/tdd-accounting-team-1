438a8065e3af71fe12c39b8ceac358f6187f0a74
Blackie Tsai <blackie1019@gmail.com>
2019-02-17 01:46:20 +0800

    Rename and arrange for fake repo

--- TDD.AccountsKata.Test/UnitTest1.cs
+++ TDD.AccountsKata.Test/UnitTest1.cs
@@ -9,8 +9,6 @@ namespace TDD.AccountsKata.Test
     [TestFixture]
     public class Tests
     {
-        private Accounting _accounting = new Accounting(new BudgetRepo());
-
         [SetUp]
         public void Setup()
         {
@@ -19,21 +17,31 @@ namespace TDD.AccountsKata.Test
         [Test]
         public void ValidateDate()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 310, YearMonth = "201901"},
+            });
+            
             var starDate = new DateTime(2019, 01, 05);
             var endDate = new DateTime(2019, 01, 04);
-            
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(0, totalAmount);
         }
-        
+
         [Test]
         public void QueryTotalWithNoBudgetRecord()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 0, YearMonth = "201904"}
+            });
+            
             var starDate = new DateTime(2019, 04, 01);
             var endDate = new DateTime(2019, 04, 02);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(0, totalAmount);
         }
@@ -41,10 +49,15 @@ namespace TDD.AccountsKata.Test
         [Test]
         public void QueryOneDateBudget()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 310, YearMonth = "201901"}
+            });
+            
             var starDate = new DateTime(2019, 01, 01);
             var endDate = new DateTime(2019, 01, 01);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(10, totalAmount);
         }
@@ -52,10 +65,15 @@ namespace TDD.AccountsKata.Test
         [Test]
         public void QueryOneMonthBudget()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 310, YearMonth = "201901"}
+            });
+            
             var starDate = new DateTime(2019, 01, 01);
             var endDate = new DateTime(2019, 01, 31);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(310, totalAmount);
         }
@@ -63,67 +81,92 @@ namespace TDD.AccountsKata.Test
         [Test]
         public void QueryTwoMonthBudget()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 280, YearMonth = "201902"},
+                new Budget {Amount = 310, YearMonth = "201903"},
+            });
+            
             var starDate = new DateTime(2019, 02, 28);
             var endDate = new DateTime(2019, 03, 01);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(20, totalAmount);
         }
         [Test]
         public void QueryTwoMonthBudgetWithNoData()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 310, YearMonth = "201903"},
+                new Budget {Amount = 0, YearMonth = "201904"}
+            });
+            
             var starDate = new DateTime(2019, 03, 30);
             var endDate = new DateTime(2019, 04, 04);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(20, totalAmount);
-        } 
-        
+        }
+
         [Test]
         public void QueryLunarYearBudget()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 290, YearMonth = "202002"}
+            });
+            
             var starDate = new DateTime(2020, 02, 01);
             var endDate = new DateTime(2020, 02, 28);
-            
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
-            
+
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
+
             AmountShouldBe(280, totalAmount);
-        }        
-        
+        }
+
         [Test]
         public void QueryThreeMonthBudget()
         {
+            var accountingInstance = GetAccountingInstance(new List<Budget>
+            {
+                new Budget {Amount = 310, YearMonth = "201901"},
+                new Budget {Amount = 280, YearMonth = "201902"},
+                new Budget {Amount = 310, YearMonth = "201903"},
+            });
+            
             var starDate = new DateTime(2019, 01, 25);
             var endDate = new DateTime(2019, 03, 05);
             
-            var totalAmount = _accounting.TotalAmount(starDate, endDate);
+            var totalAmount = accountingInstance.TotalAmount(starDate, endDate);
             
             AmountShouldBe(400, totalAmount);
         }
 
-        private static void AmountShouldBe(double expected, double totalAmount)
+        private void AmountShouldBe(double expected, double totalAmount)
         {
             Assert.AreEqual(expected, totalAmount);
         }
+        
+        private Accounting GetAccountingInstance(List<Budget> data)
+        {
+            return new Accounting(new FakeBudgetRepo(data));
+        }
     }
     
-    public class BudgetRepo : IBudgetRepo
+    public class FakeBudgetRepo : IBudgetRepo
     {
+        private readonly IList<Budget> _data;
+
+        public FakeBudgetRepo(IList<Budget> initData)
+        {
+            _data = initData;
+        }
         public IEnumerable<Budget> GetAll()
         {
-            var result = new List<Budget>
-            {
-                new Budget() {Amount = 310, YearMonth = "201901"},
-                new Budget() {Amount = 280, YearMonth = "201902"},
-                new Budget() {Amount = 310, YearMonth = "201903"},
-                new Budget() {Amount = 0, YearMonth = "201904"},
-                new Budget() {Amount = 310, YearMonth = "201912"},
-                new Budget() {Amount = 310, YearMonth = "202001"},
-                new Budget() {Amount = 290, YearMonth = "202002"},
-            };
-            return result;
+            return _data;
         }
     }
 }
